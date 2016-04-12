10d116b09950c6337d7fd72f7ef261a83c2a885b
Aana <aanabansal@gmail.com>
2016-04-12 12:57:15 -0400

    Initial Interface Commit

---
+++ hospitalist/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  hospitalist
+//
+//  Created by Aana Bansal on 4/3/16.
+//  Copyright © 2016 Aana Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UITextField!
+    @IBOutlet weak var queueButton: UIButton!
+    @IBOutlet weak var resourceStatusButton: UIButton!
+    @IBOutlet weak var newPatientButton: UIButton!
+    
+    @IBAction func queue(sender: UIButton) {
+
+    }
+
+    @IBAction func resourceStatAction(sender: AnyObject) {
+
+    }
+    
+    @IBAction func new(sender: AnyObject) {
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
